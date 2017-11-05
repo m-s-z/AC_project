d688555d65295c89f3d5f7938fe877f16c2e6302
m-s-z <m.s.z@outlook.com>
2017-11-05 20:43:43 +0100

    Test commit

---
+++ AC_project/Expert.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AC_project
+{
+    public class Expert
+    {
+        private int _index;
+        private int[] _features;
+
+        public Expert(int index)
+        {
+            _index = index;
+        }
+    }
+}
--- AC_project/Program.cs
+++ AC_project/Program.cs
@@ -1,8 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AC_project
 {
@@ -10,6 +9,46 @@ namespace AC_project
     {
         static void Main(string[] args)
         {
+            //string[] values = File.ReadLines("ac_input.txt");
+            List<DailyValues> values = File.ReadAllLines("ac_input.txt")
+                                           .Skip(1)
+                                           .Select(v => DailyValues.FromCsv(v))
+                                           .ToList();
+
+            try
+            {   // Open the text file using a stream reader.
+                using (StreamReader sr = new StreamReader("ac_input.txt"))
+                {
+                    // Read the stream to a string, and write the string to the console.
+                    string line = sr.ReadLine();
+
+                    Console.WriteLine(line);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
+        }
+
+    }
+
+    class DailyValues
+    {
+        decimal Open;
+        decimal High;
+        decimal Low;
+
+        public static DailyValues FromCsv(string csvLine)
+        {
+            string[] values = csvLine.Split(',');
+            DailyValues dailyValues = new DailyValues();
+            dailyValues.Open = Convert.ToDecimal(values[0]);
+            dailyValues.High = Convert.ToDecimal(values[1]);
+            dailyValues.Low = Convert.ToDecimal(values[2]);
+            Console.WriteLine("Open {0}, High {1}, Low {2}", dailyValues.Open, dailyValues.High, dailyValues.Low);
+            return dailyValues;
         }
     }
 }
