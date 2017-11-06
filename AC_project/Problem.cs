c7e4a67bfcb87b921f80291bd2ff46dff16ae153
Andrzej Frankowski <andrew@partyhype.com>
2017-11-06 15:26:48 +0100

    Ref 1 - Problem

---
+++ AC_project/Problem.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace AC_project
+{
+    class Problem
+    {
+        private int _numberOfFeatures;
+        private List<Project> _listProjects;
+        private List<Expert> _listExpers;
+
+        public List<Project> listProjects { get { return _listProjects; } }
+        public List<Expert> listExpers { get { return _listExpers; } }
+        public int numberOfFeatures { get { return _numberOfFeatures; } }
+        public int numberOfExperts { get { return _listProjects.Count; } }
+        public int numberOfProjects { get { return _listProjects.Count; } }
+
+        public Problem(string filePath)
+        {
+            ReadFile(filePath);
+            SetIndices();
+        }
+
+        private void ReadFile(string pathToFile)
+        {
+            try
+            {
+                string line = File.ReadLines(pathToFile).First();
+                string[] lines = line.Split(',');
+                int numberOfProjects = int.Parse(lines[0]);
+                int numberOfExperts = int.Parse(lines[1]);
+                _numberOfFeatures = int.Parse(lines[2]);
+
+                _listProjects = File.ReadLines(pathToFile)
+                                       .Skip(1)
+                                       .Take(numberOfProjects)
+                                       .Select(v => new Project(v))
+                                       .ToList();
+
+                _listExpers = File.ReadLines(pathToFile)
+                                       .Skip(numberOfProjects + 1)
+                                       .Take(numberOfExperts)
+                                       .Select(v => new Expert(v))
+                                       .ToList();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+                throw e;
+            }
+        }
+
+        private void SetIndices()
+        {
+            for (int i = 0; i < _listExpers.Count(); i++)
+            {
+                _listExpers[i].Index = i;
+            }
+
+            for (int i = 0; i < _listProjects.Count(); i++)
+            {
+                _listProjects[i].Index = i;
+            }
+        }
+    }
+}
--- AC_project/Program.cs
+++ AC_project/Program.cs
@@ -7,12 +7,9 @@ namespace AC_project
 {
     class Program
     {
-        private static int _numberOfExperts;
-        private static int _numberOfFeatures;
-        private static int _numberOfProjects;
-        private static List<Project> _listProjects;
-        private static List<Expert> _listExpers;
+        private static Problem _problem;
         private static List<Edge> _listEdges;
+
         private static Stack<int> _stackOfFeaturesPopularity;
         private static Stack<int> _stackOfProjectsDifficulty;
         private static List<Tuple<int, int>> _listOfFeaturesPopularity;
@@ -20,75 +17,31 @@ namespace AC_project
         private static double[] _arrayOfFeatureDifficulty;
         private static int _sumOfFeaturesExpertsProvide;
 
-
         static void Main(string[] args)
         {
-            ReadFile("ac_input2.txt");
+            _problem = new Problem("ac_input2.txt");
             BuildConnections();
             Console.ReadLine();
             CreateStackOfFeaturesPopularity();
             CreateStackOfProjectsDifficulty();
         }
 
-        private static void ReadFile(string pathToFile)
-        {
-            try
-            {
-                string line = File.ReadLines(pathToFile).First();
-                string[] lines = line.Split(',');
-                _numberOfProjects = int.Parse(lines[0]);
-                _numberOfExperts = int.Parse(lines[1]);
-                _numberOfFeatures = int.Parse(lines[2]);
-
-                _listProjects = File.ReadLines(pathToFile)
-                                       .Skip(1)
-                                       .Take(_numberOfProjects)
-                                       .Select(v => new Project(v))
-                                       .ToList();
-
-                _listExpers = File.ReadLines(pathToFile)
-                                       .Skip(_numberOfProjects + 1)
-                                       .Take(_numberOfExperts)
-                                       .Select(v => new Expert(v))
-                                       .ToList();
-                SetIndices();
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("The file could not be read:");
-                Console.WriteLine(e.Message);
-            }
-        }
-
-        private static void SetIndices()
-        {
-            for (int i = 0; i < _listExpers.Count(); i++)
-            {
-                _listExpers[i].Index = i;
-            }
-
-            for (int i = 0; i < _listProjects.Count(); i++)
-            {
-                _listProjects[i].Index = i;
-            }
-        }
-
         private static void BuildConnections()
         {
             _listEdges = new List<Edge>();
             _listOfFeaturesPopularity = new List<Tuple<int, int>>();
-            _arrayOfFeaturePopularity = new int[_numberOfFeatures];
-            for (int i = 0; i < _numberOfFeatures; i++)
+            _arrayOfFeaturePopularity = new int[_problem.numberOfFeatures];
+            for (int i = 0; i < _problem.numberOfFeatures; i++)
             {
-                for (int j = 0; j < _listExpers.Count(); j++)
+                for (int j = 0; j < _problem.listExpers.Count(); j++)
                 {
-                    if (_listExpers[j].HasFeature(i))
+                    if (_problem.listExpers[j].HasFeature(i))
                     {
                         _arrayOfFeaturePopularity[i]++;
                         _sumOfFeaturesExpertsProvide++;
-                        for (int k = 0; k < _listProjects.Count(); k++)
+                        for (int k = 0; k < _problem.listProjects.Count(); k++)
                         {
-                            if (_listProjects[k].HasFeature(i))
+                            if (_problem.listProjects[k].HasFeature(i))
                             {
                                 _listEdges.Add(new Edge(k, j, i));
                                 Console.WriteLine(_listEdges.Last().EdgeDescription());
@@ -123,25 +76,25 @@ namespace AC_project
 
         private static void CreateStackOfProjectsDifficulty()
         {
-            _arrayOfFeatureDifficulty = new double[_numberOfFeatures];
-            for (int i = 0; i < _numberOfFeatures; i++)
+            _arrayOfFeatureDifficulty = new double[_problem.numberOfFeatures];
+            for (int i = 0; i < _problem.numberOfFeatures; i++)
             {
                 _arrayOfFeatureDifficulty[i] = (double)_sumOfFeaturesExpertsProvide / _arrayOfFeaturePopularity[i];
             }
 
-            foreach (var project in _listProjects)
+            foreach (var project in _problem.listProjects)
             {
                 project.CalculateDifficulty(_arrayOfFeatureDifficulty);
                 Console.WriteLine("Project difficulty {0}", project.Difficulty);
                 Console.WriteLine("----------------------------");
             }
 
-            var tempList = _listProjects.OrderBy(i => i.Difficulty).ToList();
+            var tempList = _problem.listProjects.OrderBy(i => i.Difficulty).ToList();
             _stackOfProjectsDifficulty = new Stack<int>();
             foreach (var project in tempList)
             {
                 _stackOfProjectsDifficulty.Push(project.Index);
-                Console.WriteLine("Project stack {0} - {1}", _stackOfProjectsDifficulty.First(), _listProjects[_stackOfProjectsDifficulty.First()].Difficulty);
+                Console.WriteLine("Project stack {0} - {1}", _stackOfProjectsDifficulty.First(), _problem.listProjects[_stackOfProjectsDifficulty.First()].Difficulty);
             }
 
             foreach (var feature in _stackOfFeaturesPopularity)
