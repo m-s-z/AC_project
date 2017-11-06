bfb0f6ba1f14e7ce5e1030fd02374a5abcb71833
Andrzej Frankowski <andrew@partyhype.com>
2017-11-06 15:42:12 +0100

    Ref 2 - Problem Solver

---
+++ AC_project/ProblemSolver.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AC_project
+{
+    class ProblemSolver
+    {
+        Problem _problem;
+
+        private List<Edge> _listEdges;
+
+        private Stack<int> _stackOfFeaturesPopularity;
+        private Stack<int> _stackOfProjectsDifficulty;
+        private List<Tuple<int, int>> _listOfFeaturesPopularity;
+        private int[] _arrayOfFeaturePopularity;
+        private double[] _arrayOfFeatureDifficulty;
+        private int _sumOfFeaturesExpertsProvide;
+
+        public ProblemSolver(Problem problem)
+        {
+            _problem = problem;
+        }
+
+        public void Solve()
+        {
+            BuildConnections();
+            Console.ReadLine();
+            CreateStackOfFeaturesPopularity();
+            CreateStackOfProjectsDifficulty();
+        }
+
+        private void BuildConnections()
+        {
+            _listEdges = new List<Edge>();
+            _listOfFeaturesPopularity = new List<Tuple<int, int>>();
+            _arrayOfFeaturePopularity = new int[_problem.numberOfFeatures];
+            for (int i = 0; i < _problem.numberOfFeatures; i++)
+            {
+                for (int j = 0; j < _problem.listExpers.Count(); j++)
+                {
+                    if (_problem.listExpers[j].HasFeature(i))
+                    {
+                        _arrayOfFeaturePopularity[i]++;
+                        _sumOfFeaturesExpertsProvide++;
+                        for (int k = 0; k < _problem.listProjects.Count(); k++)
+                        {
+                            if (_problem.listProjects[k].HasFeature(i))
+                            {
+                                _listEdges.Add(new Edge(k, j, i));
+                                Console.WriteLine(_listEdges.Last().EdgeDescription());
+                            }
+                        }
+                    }
+                }
+                _listOfFeaturesPopularity.Add(new Tuple<int, int>(i, _arrayOfFeaturePopularity[i]));
+            }
+
+            //foreach(var p in _listOfFeaturesPopularity)
+            //{
+            //    Console.WriteLine("Feature {0}: {1}", p.Item1, p.Item2);
+            //}
+        }
+
+        private void CreateStackOfFeaturesPopularity()
+        {
+            _listOfFeaturesPopularity = _listOfFeaturesPopularity.OrderByDescending(i => i.Item2).ToList();
+            _stackOfFeaturesPopularity = new Stack<int>();
+            Console.WriteLine("Feature podaż {0}", _sumOfFeaturesExpertsProvide);
+            foreach (var p in _listOfFeaturesPopularity)
+            {
+                Console.WriteLine("Feature {0}: {1}", p.Item1, p.Item2);
+                _stackOfFeaturesPopularity.Push(p.Item1);
+            }
+            foreach (var p in _stackOfFeaturesPopularity)
+            {
+                Console.WriteLine("Stack {0}", p);
+            }
+        }
+
+        private void CreateStackOfProjectsDifficulty()
+        {
+            _arrayOfFeatureDifficulty = new double[_problem.numberOfFeatures];
+            for (int i = 0; i < _problem.numberOfFeatures; i++)
+            {
+                _arrayOfFeatureDifficulty[i] = (double)_sumOfFeaturesExpertsProvide / _arrayOfFeaturePopularity[i];
+            }
+
+            foreach (var project in _problem.listProjects)
+            {
+                project.CalculateDifficulty(_arrayOfFeatureDifficulty);
+                Console.WriteLine("Project difficulty {0}", project.Difficulty);
+                Console.WriteLine("----------------------------");
+            }
+
+            var tempList = _problem.listProjects.OrderBy(i => i.Difficulty).ToList();
+            _stackOfProjectsDifficulty = new Stack<int>();
+            foreach (var project in tempList)
+            {
+                _stackOfProjectsDifficulty.Push(project.Index);
+                Console.WriteLine("Project stack {0} - {1}", _stackOfProjectsDifficulty.First(), _problem.listProjects[_stackOfProjectsDifficulty.First()].Difficulty);
+            }
+
+            foreach (var feature in _stackOfFeaturesPopularity)
+            {
+                Console.WriteLine("Feature stack {0} - {1}", feature, _arrayOfFeaturePopularity[feature]);
+            }
+        }
+    }
+}
--- AC_project/Program.cs
+++ AC_project/Program.cs
@@ -19,88 +19,9 @@ namespace AC_project
 
         static void Main(string[] args)
         {
-            _problem = new Problem("ac_input2.txt");
-            BuildConnections();
-            Console.ReadLine();
-            CreateStackOfFeaturesPopularity();
-            CreateStackOfProjectsDifficulty();
-        }
-
-        private static void BuildConnections()
-        {
-            _listEdges = new List<Edge>();
-            _listOfFeaturesPopularity = new List<Tuple<int, int>>();
-            _arrayOfFeaturePopularity = new int[_problem.numberOfFeatures];
-            for (int i = 0; i < _problem.numberOfFeatures; i++)
-            {
-                for (int j = 0; j < _problem.listExpers.Count(); j++)
-                {
-                    if (_problem.listExpers[j].HasFeature(i))
-                    {
-                        _arrayOfFeaturePopularity[i]++;
-                        _sumOfFeaturesExpertsProvide++;
-                        for (int k = 0; k < _problem.listProjects.Count(); k++)
-                        {
-                            if (_problem.listProjects[k].HasFeature(i))
-                            {
-                                _listEdges.Add(new Edge(k, j, i));
-                                Console.WriteLine(_listEdges.Last().EdgeDescription());
-                            }
-                        }
-                    }
-                }
-                _listOfFeaturesPopularity.Add(new Tuple<int, int>(i, _arrayOfFeaturePopularity[i]));
-            }
-
-            //foreach(var p in _listOfFeaturesPopularity)
-            //{
-            //    Console.WriteLine("Feature {0}: {1}", p.Item1, p.Item2);
-            //}
-        }
-
-        private static void CreateStackOfFeaturesPopularity()
-        {
-            _listOfFeaturesPopularity = _listOfFeaturesPopularity.OrderByDescending(i => i.Item2).ToList();
-            _stackOfFeaturesPopularity = new Stack<int>();
-            Console.WriteLine("Feature podaż {0}", _sumOfFeaturesExpertsProvide);
-            foreach (var p in _listOfFeaturesPopularity)
-            {
-                Console.WriteLine("Feature {0}: {1}", p.Item1, p.Item2);
-                _stackOfFeaturesPopularity.Push(p.Item1);
-            }
-            foreach (var p in _stackOfFeaturesPopularity)
-            {
-                Console.WriteLine("Stack {0}", p);
-            }
-        }
-
-        private static void CreateStackOfProjectsDifficulty()
-        {
-            _arrayOfFeatureDifficulty = new double[_problem.numberOfFeatures];
-            for (int i = 0; i < _problem.numberOfFeatures; i++)
-            {
-                _arrayOfFeatureDifficulty[i] = (double)_sumOfFeaturesExpertsProvide / _arrayOfFeaturePopularity[i];
-            }
-
-            foreach (var project in _problem.listProjects)
-            {
-                project.CalculateDifficulty(_arrayOfFeatureDifficulty);
-                Console.WriteLine("Project difficulty {0}", project.Difficulty);
-                Console.WriteLine("----------------------------");
-            }
-
-            var tempList = _problem.listProjects.OrderBy(i => i.Difficulty).ToList();
-            _stackOfProjectsDifficulty = new Stack<int>();
-            foreach (var project in tempList)
-            {
-                _stackOfProjectsDifficulty.Push(project.Index);
-                Console.WriteLine("Project stack {0} - {1}", _stackOfProjectsDifficulty.First(), _problem.listProjects[_stackOfProjectsDifficulty.First()].Difficulty);
-            }
-
-            foreach (var feature in _stackOfFeaturesPopularity)
-            {
-                Console.WriteLine("Feature stack {0} - {1}", feature, _arrayOfFeaturePopularity[feature]);
-            }
+            Problem problem = new Problem("ac_input2.txt");
+            ProblemSolver solver = new ProblemSolver(problem);
+            solver.Solve();
         }
     }
 }
