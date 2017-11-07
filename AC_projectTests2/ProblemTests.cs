67f003a7a3ae55fc7d2045a2ce206fbf1040a189
Andrzej Frankowski <andrew@partyhype.com>
2017-11-07 03:08:47 +0100

    Problem tests. Expert tests.

--- AC_projectTests2/ProblemTests.cs
+++ AC_projectTests2/ProblemTests.cs
@@ -57,13 +57,46 @@ namespace AC_project.Tests
         [TestMethod()]
         public void SetIndicesTest()
         {
-            Assert.Inconclusive();
-        }
+            int numberOfFeatures = 5;
+            List<Project> projects = new List<Project>();
+            for (int i = 0; i < 5; i++)
+            {
+                String line = "";
+                for (int f = 0; f < numberOfFeatures; f++)
+                {
+                    line += String.Format("{0}", i);
+                    if (f < numberOfFeatures - 1)
+                    {
+                        line += ",";
+                    }
+                }
+                Project p = new Project(line);
+                projects.Add(p);
+            }
 
-        [TestMethod()]
-        public void ReadFileTest()
-        {
-            Assert.Inconclusive();
+            List<Expert> experts = new List<Expert>();
+            for (int i = 0; i < 5; i++)
+            {
+                String line = "";
+                for (int f = 0; f < numberOfFeatures; f++)
+                {
+                    line += String.Format("{0}", i);
+                    if (f < numberOfFeatures - 1)
+                    {
+                        line += ",";
+                    }
+                }
+                Expert e = new Expert(line);
+                experts.Add(e);
+            }
+
+            Problem problem = new Problem(projects, experts);
+            problem.SetIndices();
+
+            Assert.AreEqual(3, problem.listProjects[3].Index);
+            Assert.AreEqual(0, problem.listProjects[0].Index);
+            Assert.AreEqual(3, problem.listExperts[3].Index);
+            Assert.AreEqual(0, problem.listExperts[0].Index);
         }
     }
 }
